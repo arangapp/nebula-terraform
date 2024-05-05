b36569471db2f4bef34d919858ecd29f4af8ab09
Rangappagari <anjineyulu.rangappagari@capgemini.com>
2024-05-05 23:35:56 +0800

    new config

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -1,12 +1,24 @@
 provider "aws" {
   region = "us-east-1"
 }
+#
+#resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.0.0.0/16"
+#    tags = {
+#   Name      = "myvpc"  # Set your desired vpc name here
+#    "Terraform" = "Yes"
+#  }
+  
+#}
 
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
-    Name      = "myvpc"  # Set your desired vpc name here
-    "Terraform" = "Yes"
+    Name = var.inputname
+    }
   }
-  
-}
+
+  output "vpcid" {
+    value = aws_vpc.myvpc.id
+    
+  }
